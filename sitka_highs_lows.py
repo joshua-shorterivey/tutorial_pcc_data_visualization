527941916ea74f720c2de839c7a54046564d4c27
joshua-shorterivey <joshua.shorterivey@gmail.com>
2023-01-11 11:54:43 -0700

    added additional modules

--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -13,13 +13,15 @@ with open(filename) as f:
     # for index, column_header in enumerate(header_row):
     #     print(index, column_header)
 
-    # get dates and  high temperatures from this file.
-    dates, highs = [], []
+    # get dates, high, and low temperatures from this file.
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
 print (highs)
 
@@ -27,9 +29,11 @@ print (highs)
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 #format plot
-ax.set_title('Daily high temperatures - 2018', fontsize=24)
+ax.set_title('Daily high and low temperatures - 2018', fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperatures (F)', fontsize=16)
