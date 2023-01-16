415e09c44faa11aba188863f90320ab6f80d5156
joshua-shorterivey <joshua.shorterivey@gmail.com>
2023-01-16 16:02:36 -0700

    added modules

--- ch16_try_it_yourself.py
+++ ch16_try_it_yourself.py
@@ -155,3 +155,14 @@ plt.show()
 
 # 16-5. Explore: Generate a few more visualizations that examine any other
 # weather aspect you’re interested in for any locations you’re curious about.
+
+
+# 16-6. Refactoring: The loop that pulls data from all_eq_dicts uses variables for the magnitude, longitude, latitude, and title of each earthquake before append- ing these values to their appropriate lists. This approach was chosen for clar- ity in how to pull data from a JSON file, but it’s not necessary in your code. Instead of using these temporary variables, pull each value from eq_dict and append it to the appropriate list in one line. Doing so should shorten the body of this loop to just four lines.
+
+# 16-7. Automated Title: In this section, we specified the title manually when defin- ing my_layout, which means we have to remember to update the title every time the source file changes. Instead, you can use the title for the data set in the metadata part of the JSON file. Pull this value, assign it to a variable, and use this for the title of the map when you’re defining my_layout.
+
+
+# 16-8. Recent Earthquakes: You can find data files containing information about the most recent earthquakes over 1-hour, 1-day, 7-day, and 30-day periods online. Go to https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php and you’ll see a list of links to data sets for various time periods, focusing on earthquakes of different magnitudes. Download one of these data sets, and create a visualization of the most recent earthquake activity.
+
+# 16-9. World Fires: In the resources for this chapter, you’ll find a file called world_fires_1_day.csv. This file contains information about fires burning in dif- ferent locations around the globe, including the latitude and longitude, and the brightness of each fire. Using the data processing work from the first part of this chapter and the mapping work from this section, make a map that shows which parts of the world are affected by fires.
+# You can download more recent versions of this data at https://earthdata .nasa.gov/earth-observation-data/near-real-time/firms/active-fire-data/. You can find links to the data in CSV format in the TXT section.
