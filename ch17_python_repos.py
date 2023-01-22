1b0e45b5797038d77b91dcf0b1d7bc2b85c9dff6
joshua-shorterivey <joshua.shorterivey@gmail.com>
2023-01-22 16:28:51 -0700

    added chapter 17 file

---
+++ ch17_python_repos.py
@@ -0,0 +1,34 @@
+import requests
+
+#make an api cal and store the response
+url = "https://api.github.com/search/repositories?q=langauge:python&sort=stars"
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+#store api response in a variable.
+response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+
+#process results. 
+#print(response_dict.keys())
+
+#explore information about the repositories
+repo_dicts = response_dict['items']
+print((f"Repositories returned: {len(repo_dicts)}"))
+
+#examine the first repository. 
+repo_dict = repo_dicts[0]
+# print(f'\nKeys: {len(repo_dict)}')
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+
+#printing selected info for certain keys
+print("\nSelected information about first repository:")
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Stars: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
